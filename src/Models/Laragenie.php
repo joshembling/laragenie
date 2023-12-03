498d7accec9063a0e1b9302e44df46588c5a0cd9
Josh Embling <joshembling@gmail.com>
2023-12-03 14:44:15 +0000

    update model and migration

--- src/Models/Laragenie.php
+++ src/Models/Laragenie.php
@@ -1,10 +1,19 @@
 <?php
 
-namespace App\Models;
+namespace JoshEmbling\Laragenie\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Laragenie extends Model
 {
     protected $table = 'laragenie_responses';
+
+    protected $fillable = [
+        'question', 'answer', 'cost', 'vectors',
+    ];
+
+    protected $casts = [
+        'cost' => 'double',
+        'vectors' => 'array',
+    ];
 }
